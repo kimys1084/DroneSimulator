fixed rotation probelm

quadcopter-simulation/controller3.py
@@ -15,16 +15,16 @@ v_p = 1000
 k_p = np.array([[v_p,0,0],
 				[0, v_p,0],
 				[0,0,v_p]])
-v_v = 2
+v_v = 20
 k_v = np.array([[v_v,0,0],
 				[0,v_v,0],
 				[0,0,v_v]])
-v_r=0.003
+v_r= 3
 k_r = np.array([[v_r,0,0],
 				[0, v_r,0],
 				[0,0,v_r]])
 
-v_w = 1
+v_w =2
 k_w = np.array([[v_w,0,0],
 				[0,v_w,0],
 				[0,0,v_w]])
@@ -70,7 +70,7 @@ def run(quad, des_state):
 	y_b = normalize(v_cross(z_b, x_c))
 	x_b = normalize(v_cross(y_b, z_b))
 	w_R_b = make_R(x_b, y_b, z_b)
-	
+ 	W_R_b = quad.rotation()
 
 	des_psi = des_state.yaw
 	des_psi_dot = des_state.yawdot
@@ -93,7 +93,7 @@ def run(quad, des_state):
 	x_b_des = v_cross(y_b_des, z_b_des)
 
 	R_des = make_R(x_b_des, y_b_des, z_b_des)
-
+	#R_des = np.array([[1,0,0],[0,1,0],[0,0,1]])
 	#quad.store_xyz(x_b_des, y_b_des, z_b_des)	
 	quad.store_xyz(x_b, z_b_des, z_b)	
 	#print ""
@@ -102,9 +102,8 @@ def run(quad, des_state):
 	
 	w_des = np.array([[0,0,0]]).T
 	e_w = w_b - w_des
-	M = -np.dot(k_r,e_R) #- np.dot(k_w,e_w)
 
-	'''
+	'''	
 	if int(quad.get_index()) == 1: #yaw +
 		M = np.array([[0,0,10]]).T
 		quad.set_index(5)
@@ -118,12 +117,12 @@ def run(quad, des_state):
 		M = np.array([[-1,0,0]]).T
 		quad.set_index(5)
 	else:							 #default
-		M = np.array([[0,0,0]]).T
+		#M = np.array([[0,0,0]]).T
+		M = -np.dot(k_r,e_R) - np.dot(k_w,e_w)
 	'''
 
+	M = -np.dot(k_r,e_R) - np.dot(k_w,e_w)
 	F = np.dot(F_des.T, z_b)[0][0]
-	print "before F: ", F
-	print "before M: ", M
 	if debug == True:
 		print "R_des", R_des
 		print ""

quadcopter-simulation/model/quadcopter.py
@@ -75,7 +75,9 @@ class Quadcopter:
 	def attitude(self):
 		rot = Quaternion(self.state[6:10]).as_rotation_matrix()
 		return RotToRPY(rot)
-    
+   	def rotation(self):
+		return Quaternion(self.state[6:10]).as_rotation_matrix()
+ 
 	def omega(self):
 		return self.state[10:13]
 
@@ -106,7 +108,7 @@ class Quadcopter:
 		quat = np.array([qw,qx,qy,qz])
 
 		bRw = Quaternion(quat).as_rotation_matrix() # world to body rotation matrix
-		wRb = bRw.T # orthogonal matrix inverse = transpose
+		wRb = bRw # orthogonal matrix inverse = transpose
         # acceleration - Newton's second law of motion
 		accel = 1.0 / params.mass * (wRb.dot(np.array([[0, 0, F]]).T) - np.array([[0, 0, params.mass * params.g]]).T)
 		
@@ -117,7 +119,7 @@ class Quadcopter:
 		quat = np.array([qw,qx,qy,qz])
 
 		bRw = Quaternion(quat).as_rotation_matrix() # world to body rotation matrix
-		wRb = bRw.T # orthogonal matrix inverse = transpose
+		wRb = bRw # orthogonal matrix inverse = transpose
         # acceleration - Newton's second law of motion
 		accel = 1.0 / params.mass * (wRb.dot(np.array([[0, 0, F]]).T) - np.array([[0, 0, params.mass * params.g]]).T)
         # angular velocity - using quternion

quadcopter-simulation/utils/utils.py
@@ -2,9 +2,9 @@ import numpy as np
 from math import sin, cos, asin, atan2, sqrt
 
 def RotToRPY(R):
-    phi = asin(R[1,2])
-    theta = atan2(-R[0,2]/cos(phi),R[2,2]/cos(phi))
-    psi = atan2(-R[1,0]/cos(phi),R[1,1]/cos(phi))
+    phi = asin(R[2,1])
+    theta = atan2(-R[2,0]/cos(phi),R[2,2]/cos(phi))
+    psi = atan2(-R[0,1]/cos(phi),R[1,1]/cos(phi))
     return phi, theta, psi
 
 def RPYToRot(phi, theta, psi):
@@ -14,7 +14,7 @@ def RPYToRot(phi, theta, psi):
     """
     return np.array([[cos(psi)*cos(theta) - sin(phi)*sin(psi)*sin(theta), cos(theta)*sin(psi) + cos(psi)*sin(phi)*sin(theta), -cos(phi)*sin(theta)],
                      [-cos(phi)*sin(psi), cos(phi)*cos(psi), sin(phi)],
-                     [cos(psi)*sin(theta) + cos(theta)*sin(phi)*sin(psi), sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi), cos(phi)*cos(theta)]])
+                     [cos(psi)*sin(theta) + cos(theta)*sin(phi)*sin(psi), sin(psi)*sin(theta) - cos(psi)*cos(theta)*sin(phi), cos(phi)*cos(theta)]]).T
 
 def RotToQuat(R):
     """
@@ -28,26 +28,26 @@ def RotToQuat(R):
     if tr > 0:
         S = sqrt(tr+1.0) * 2 # S=4*qw
         qw = 0.25 * S
-        qx = (R[2,1] - R[1,2]) / S
-        qy = (R[0,2] - R[2,0]) / S
-        qz = (R[1,0] - R[0,1]) / S
-    elif (R[0,1] > R[1,1]) and (R[0,0] > R[2,2]):
+        qx = (R[1,2] - R[2,1]) / S
+        qy = (R[2,0] - R[0,2]) / S
+        qz = (R[0,1] - R[1,0]) / S
+    elif (R[1,0] > R[1,1]) and (R[0,0] > R[2,2]):
         S = sqrt(1.0 + R[0,0] - R[1,1] - R[2,2]) * 2 # S=4*qx
-        qw = (R[2,1] - R[1,2]) / S
+        qw = (R[1,2] - R[2,1]) / S
         qx = 0.25 * S
-        qy = (R[0,1] + R[1,0]) / S
-        qz = (R[0,2] + R[2,0]) / S
+        qy = (R[1,0] + R[0,1]) / S
+        qz = (R[2,0] + R[0,2]) / S
     elif R[1,1] > R[2,2]:
         S = sqrt(1.0 + R[1,1] - R[0,0] - R[2,2]) * 2 # S=4*qy
-        qw = (R[0,2] - R[2,0]) / S
-        qx = (R[0,1] + R[1,0]) / S
+        qw = (R[2,0] - R[0,2]) / S
+        qx = (R[1,0] + R[0,1]) / S
         qy = 0.25 * S
-        qz = (R[1,2] + R[2,1]) / S
+        qz = (R[2,1] + R[1,2]) / S
     else:
         S = sqrt(1.0 + R[2,2] - R[0,0] - R[1,1]) * 2 # S=4*qz
-        qw = (R[1,0] - R[0,1]) / S
-        qx = (R[0,2] + R[2,0]) / S
-        qy = (R[1,2] + R[2,1]) / S
+        qw = (R[0,1] - R[1,0]) / S
+        qx = (R[2,0] + R[0,2]) / S
+        qy = (R[2,1] + R[1,2]) / S
         qz = 0.25 * S
     
     q = np.sign(qw) * np.array([qw, qx, qy, qz])